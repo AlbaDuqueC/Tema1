4d38b12998dd1ba3d2c45ae304202658952c911f
alba.duque <alba.duque@106-09.info2>
2024-09-25 09:15:25 +0200

    actualizacion

---
+++ Tema1/src/parte1/Ejercicio11.java
@@ -0,0 +1,25 @@
+package parte1;
+
+import java.util.Scanner;
+
+public class Ejercicio11 {
+
+	public static void main(String[] args) {
+		//Crea el escaner 
+		Scanner reader  = new Scanner(System.in);
+				
+		//Pide al usuario su edad
+		System.out.println("Introduzca la cantidad en pesetas:");
+				
+		//Introduce la edad mediante la console
+		double pesetas = reader.nextDouble();
+		
+		//cambio de pesetas a euros
+		double euros = (pesetas/166);
+		
+		//Escribe la cifra
+		System.out.println("Serian "+euros+" euros.");
+
+	}
+
+}
---
+++ Tema1/src/parte1/Ejercicio12.java
@@ -0,0 +1,10 @@
+package parte1;
+
+public class Ejercicio12 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
