38a7d09f8e25196f88652d38ebf7bc20c44ffe5c
albad <albad@192.168.1.29>
2024-09-30 18:15:54 +0200

    3

--- Tema1/src/parte2/Ejercicio3.java
+++ Tema1/src/parte2/Ejercicio3.java
@@ -9,28 +9,31 @@ public class Ejercicio3 {
 		int numA;
 		int resto;
 		int numB;
+		int resultado;
 		int result;
 		
 		//crea el scanner
 		Scanner reader= new Scanner(System.in);
 		
 		//Pide que introduzcas un numero
-		System.out.println("Introduzca un numero");
+		System.out.println("Introduzca dos numeros");
 		
 		//introducir el numero
 		numA= reader.nextInt();
+		//introducir el numero
+		numB= reader.nextInt();
 		
-		/*Formula: conseguimnos el resto dividiendo numA con 7
-		 * Teniendo el resto calculamos el numB restandole el resto a 7*/
+		/*Formula: conseguimnos el resto dividiendo numA con numB
+		 * Teniendo el resto calculamos el resultado restandole el resto a numB*/
 		
 		//calcular el resto
-		resto= numA%7;
+		resto= numA%numB;
 		
 		//calculamos el numB
-		numB=7-resto;
+		resultado=numB-resto;
 		
 		//Calcular el resultado
-		result= numB==7 ? 0:numB;
+		result= resultado==numB ? 0:resultado;
 		
 		//te dice el resultado final
 		System.out.println(result);
